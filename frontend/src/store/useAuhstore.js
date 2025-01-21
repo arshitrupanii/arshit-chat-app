c1e2356eda5c9b75b5fb07edc87ad542fa754e78
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-01-21 09:43:07 +0530

    update

--- frontend/src/store/useAuhstore.js
+++ frontend/src/store/useAuhstore.js
@@ -1,8 +1,8 @@
 import { create } from 'zustand'
 import { Axiosinstance } from '../lib/axios'
+import { toast } from 'react-hot-toast'
 
 
-const BASE_URL = 'http://localhost:4000 '
 export const useAuthStore = create((set) => ({
     authUser: null,
     isSignedIn: false,
@@ -23,30 +23,5 @@ export const useAuthStore = create((set) => ({
         }
     },
 
-    signup : async (userData) => {
-        set({ isSigningUp: true });
-        try {
-          const response = await fetch(`${BASE_URL}/api/auth/signup`, {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(userData),
-          });
-      
-          if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-          }
-      
-          const data = await response.json();
-          set({ authUser: data.user, token: data.token });
-          toast.success("Signup successful");
-          get().connectSocket();
-        } catch (error) {
-          console.error("Error during signup:", error);
-          toast.error(error.message || "Signup failed");
-        } finally {
-          set({ isSigningUp: false });
-        }
-      },
+   
 }))
