f3ce6c72fd674372b369751819b89cade1e35aef
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-06-02 14:04:05 +0530

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -92,7 +92,7 @@ export const login = async (req, res) => {
 
     } catch (error) {
         console.log("error in login ::", error);
-        res.status(500).json({ message: 'Internal server error in login' });
+        res.status(500).json({ message: error});
     }
 
 
