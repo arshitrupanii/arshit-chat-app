b8aaaebb00e079145f43df630bfc1932b6b71393
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-06-02 10:43:21 +0530

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    minify: true,
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
-  },
 })
