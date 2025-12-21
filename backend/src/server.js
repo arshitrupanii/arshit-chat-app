f3182a9503c451117d45e92635e2262ddbfbcc0f
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-12-21 12:09:14 +0530

    update url

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,9 +7,6 @@ import authRoutes from './routes/auth.routes.js';
 import messageRoutes from './routes/message.routes.js';
 import { connectDB } from './lib/db.js';
 import { app, server } from "./lib/socket.js";
-import path from 'path';
-
-const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -31,13 +28,9 @@ app.use(cors({
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 
-if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', "dist", "index.html"));
-  });
-}
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
 
 server.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const Axiosinstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "https://arshit-chat-app-backend.vercel.app/api",
     withCredentials: true
 });
